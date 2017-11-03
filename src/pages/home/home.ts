dd2f940da133bb56f48322683583070acb31bb13
olofvi <olofvi89@gmail.com>
2017-11-03 14:15:45 +0100

    removed double qoutes in home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { MapPage} from "../map/map";
+import { MapPage} from '../map/map';
 
 @Component({
   selector: 'page-home',
